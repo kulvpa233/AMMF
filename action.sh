3e5375fef39d908d4084bf65e955fb40057cee42
Aurora-Nasa-1 <b04031010@outlook.com>
2025-03-16 08:45:04 +0800

    fix action

--- action.sh
+++ action.sh
@@ -3,32 +3,6 @@
 ACTION_PATH="$NOW_PATH/AMMF/settings/script/User_START.sh"
 NOW_PATH="/data/local/tmp"
 MODPATH=${0%/*}
-detect_environment() {
-    ENVIRONMENT="UNKNOWN"
-    BUSYBOX_PATH=""
-
-    if [ -d "/data/adb/magisk" ]; then
-        ENVIRONMENT="MAGISK"
-        BUSYBOX_PATH="/data/adb/magisk/busybox"
-    fi
-
-    if [ -d "/data/adb/ksu" ]; then
-        ENVIRONMENT="KERNELSU"
-        BUSYBOX_PATH="/data/adb/ksu/bin/busybox"
-    fi
-
-    if [ -d "/data/adb/ap" ]; then
-        ENVIRONMENT="APATCH"
-        BUSYBOX_PATH="/data/adb/ap/bin/busybox"
-    fi
-    if [ "$ENVIRONMENT" = "UNKNOWN" ]; then
-        echo "UNKNOWN ENVIRONMENT"
-        exit 1
-    fi
-    echo "Environment: $ENVIRONMENT"
-    echo "BusyBox path: $BUSYBOX_PATH"
-}
-detect_environment
 VERSION=$(grep "version" "$MODPATH/module.prop" | awk -F'=' '{print $2}' | awk 'NR==1')
 echo "Module Version: $VERSION"
 echo ""
@@ -36,4 +10,4 @@ mkdir -p "$NOW_PATH"/AMMF/
 cp -r "$MODPATH"/* "$NOW_PATH"/AMMF/
 chmod -R 755 "$NOW_PATH"/AMMF/
 
-ASH_STANDALONE=1 $BUSYBOX_PATH sh "$ACTION_PATH" "$MODPATH" "$NOW_PATH/AMMF" "Action"
+sh "$ACTION_PATH" "$MODPATH" "$NOW_PATH/AMMF" "Action"
